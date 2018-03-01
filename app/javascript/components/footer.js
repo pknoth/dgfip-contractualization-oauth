23eaeabbecff72ac3c95e80f13a8c3963c73ec5a
Pascal Knoth <pknoth@octo.com>
2018-03-01 17:09:04 +0100

    updates footer

--- app/javascript/components/footer.js
+++ app/javascript/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className='footer'>
     <div className='container'>
       <div className='footer__logo'>
-        <img src='http://next.particulier.api.gouv.fr/static/images/etalab.svg' alt='Logo Etalab' />
+        <img src='http://next.particulier.api.gouv.fr/static/images/logo-api.gouv.fr-white.png' alt='Logo Etalab' />
         <ul className='footer__social'>
           <li><a href='https://twitter.com/etalab' title='Twitter'><img src='http://next.particulier.api.gouv.fr/static/images/social/twitter.svg' alt='Twitter' className='icon icon-twitter' /></a></li>
           <li><a href='https://github.com/etalab' title='Github'><img src='http://next.particulier.api.gouv.fr/static/images/social/github.svg' alt='Github' className='icon icon-github' /></a></li>
