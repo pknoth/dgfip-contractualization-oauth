951a7a564bbc20eefb5eed94382ee318d931d4f0
Pascal Knoth <pknoth@octo.com>
2018-03-09 17:17:11 +0100

    fixes images

--- app/javascript/components/header.js
+++ app/javascript/components/header.js
@@ -6,7 +6,7 @@ const Header = () => (
     <nav className='nav'>
       <div className='nav__container'>
         <a className='nav__link' href='/'>
-          <img className='nav__logo' src='http://next.particulier.api.gouv.fr/static/images/logo-api-particulier.png' alt='Accueil de particulier.api.data.gouv.fr' />
+          <img className='nav__logo' src='https://next.particulier.api.gouv.fr/static/images/logo-api-particulier.png' alt='Accueil de particulier.api.data.gouv.fr' />
         </a>
         <ul className='nav__links'>
           <li><Link href='/about'><a>A propos</a></Link></li>
