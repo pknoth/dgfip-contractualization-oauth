8696ba6ed037a4b55432467e238821f7d8616fc4
Pascal Knoth <pknoth@octo.com>
2018-03-09 16:21:53 +0100

    redirect on signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_up_path_for(_resource)
+    stored_location_for(:user)
+  end
+
   def after_sign_out_path_for(_resource)
     new_user_session_path
   end
