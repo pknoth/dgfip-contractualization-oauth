c0ef79caf77180c3aa886cd0767dd8a94e92ff27
Pascal Knoth <pknoth@octo.com>
2018-06-01 11:54:21 +0200

    add api_entreprise scope

--- app/models/account_type.rb
+++ app/models/account_type.rb
@@ -2,7 +2,7 @@ class AccountType < ApplicationRecord
   has_many :users
 
   class << self
-    %w[service_provider api_particulier dgfip cnaf].each do |role|
+    %w[service_provider api_particulier api_entreprise dgfip cnaf].each do |role|
       define_method(role.to_sym) do
         find_by(name: role)
       end
